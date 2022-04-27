2f19dbb03edbf2611dc837fb87869e48d2003fcc
JoaoTony <joaojosetony2016@gmail.com>
2022-04-27 17:26:01 +0200

    feat: add settings screen

--- screens/home/settings/index.tsx
+++ screens/home/settings/index.tsx
@@ -1,9 +1,43 @@
 import React, { FC } from 'react'
+import Button from '../../../components/button'
+import { Dimensions } from 'react-native'
 
-import { Container } from './settings.styles'
+import {
+  Container,
+  Avatar,
+  Title,
+  UserInformationWrapper,
+  UserInformationTitle,
+  UserInformationText
+} from './settings.styles'
+
+const width = Dimensions.get('screen').width
 
 const Settings: FC = () => (
-  <Container></Container>
+  <Container>
+     <Avatar source={require('../../../assets/avatar.jpg')} />
+     <Title>Bartolomeu Kuma</Title>
+
+     <UserInformationWrapper>
+       <UserInformationTitle>Nome</UserInformationTitle>
+       <UserInformationText>Bartolomeu Kuma</UserInformationText>
+
+       <UserInformationTitle>Telefone</UserInformationTitle>
+       <UserInformationText>999 999 999</UserInformationText>
+
+       <UserInformationTitle>N do BI</UserInformationTitle>
+       <UserInformationText>Lda21892938924KZ</UserInformationText>
+
+       <UserInformationTitle>Carta de Condução</UserInformationTitle>
+       <UserInformationText>45sdhk855344</UserInformationText>
+     </UserInformationWrapper>
+
+     <Button
+        width={(width - 32).toString()}
+        onPress={() => {}}
+        text="Terminar Sessaão"
+      />
+  </Container>
 )
 
 export default Settings
--- screens/home/settings/settings.styles.ts
+++ screens/home/settings/settings.styles.ts
@@ -8,6 +8,35 @@ export const Container = styled.View`
 
   padding: 0 16px;
 
-  position: relative;
+  align-items: center;
+  justify-content: flex-start;
+  padding-top: 100px;
+`
+export const Avatar = styled.Image`
+  width: 90px;
+  height: 90px;
+  border-radius: 45px;
+`
+export const Title = styled.Text`
+  font-size: 22px;
+  font-family: Poppins_700Bold;
+  color: #000;
+`
+export const UserInformationWrapper = styled.View`
+  align-items: flex-start;
+  width: 100%;
+  margin-top: 40px;
+  margin-bottom: 30px;
+`
+export const UserInformationTitle = styled.Text`
+  font-size: 16px;
+  font-family: Poppins_700Bold;
+  color: #3D3D3D;
+`
 
+export const UserInformationText = styled.Text`
+  font-size: 16px;
+  font-family: Poppins_400Regular;
+  color: #3D3D3D;
+  margin-bottom: 10px;
 `
