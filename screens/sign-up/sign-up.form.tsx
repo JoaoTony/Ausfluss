29bc99c6d4db0610b1f690c7ce50ac8b7d5e6d74
JoaoTony <joaojosetony2016@gmail.com>
2022-04-28 00:12:18 +0200

    feat: add sing in and sign up functions

--- screens/sign-up/sign-up.form.tsx
+++ screens/sign-up/sign-up.form.tsx
@@ -1,10 +1,11 @@
+/* eslint-disable no-undef */
 // @ts-nocheck
 
 import { FC, useState } from 'react'
 import BackIcon from '../../assets/back-icon.png'
 import { useNavigation, NavigationProp } from '@react-navigation/native'
 import { RootStackParam } from '../../types/root-stack.type'
-import { Dimensions } from 'react-native'
+import { Dimensions, Alert } from 'react-native'
 
 import {
   Form,
@@ -23,10 +24,50 @@ const SignUpForm: FC = () => {
   const [slideIndex, setSlideIndex] = useState(0)
   const width = Dimensions.get('screen').width
   const navigation: NavitaionStak = useNavigation()
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [values, setValues] = useState({
+    name: '',
+    phone: '999999999',
+    num_BI: '',
+    password: '',
+    address: 'some_address',
+    driver_id: '',
+    email: '',
+    driver_type: 'some_driver'
+  })
+
+  const handleSignUp = async () => {
+    if (confirmPassword === values.password) {
+      try {
+        const response = await fetch('http://192.168.0.105:8080/api/v1/drivers', {
+          method: 'POST',
+          body: JSON.stringify(values),
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+          }
+        })
+
+        const res = await response.json()
+
+        if (response.status === 201) {
+          console.log('201: ', response)
+          navigation.navigate('Login')
+        } else {
+          console.log('N 201: ', res)
+        }
+      } catch (error) {
+        console.log('Erro: ', error)
+        Alert.alert('Erro', 'Erro no catch')
+      }
+    } else {
+      Alert.alert('Palavra passe', 'A palavras-pases diferentes')
+    }
+  }
 
   const handleNext = () => {
     if (slideIndex === 1) {
-      navigation.navigate('Home')
+      handleSignUp()
     } else setSlideIndex(prev => prev + 1)
   }
 
@@ -37,16 +78,40 @@ const SignUpForm: FC = () => {
       {slideIndex === 0
         ? (
         <>
-          <Input placeholder="Digite o Nome" width={(width - 32).toString()}/>
-          <Input placeholder="Digete 0 Telefone" width={(width - 32).toString()} />
-          <Input placeholder="Digete o N do BI" width={(width - 32).toString()} />
-          <Input placeholder="Digete a Carta de Condiução" width={(width - 32).toString()} />
+          <Input
+            placeholder="Digite o Nome"
+            width={(width - 32).toString()}
+            onChangeText={(value) => setValues(prev => ({ ...prev, name: value }))}
+          />
+          <Input
+            placeholder="Digete o Email"
+            width={(width - 32).toString()}
+            onChangeText={(value) => setValues(prev => ({ ...prev, email: value }))}
+          />
+          <Input
+            placeholder="Digete o N do BI"
+            width={(width - 32).toString()}
+            onChangeText={(value) => setValues(prev => ({ ...prev, num_BI: value }))}
+          />
+          <Input
+            placeholder="Digete a Carta de Condiução"
+            width={(width - 32).toString()}
+            onChangeText={(value) => setValues(prev => ({ ...prev, driver_id: value }))}
+          />
         </>
           )
         : (
         <>
-          <Input placeholder="Digite o palavra-passe" width={(width - 32).toString()} secureTextEntry={true} />
-          <Input placeholder="Confirmar a palavra-passe" width={(width - 32).toString()} secureTextEntry={true} />
+          <Input
+            placeholder="Digite o palavra-passe"
+            width={(width - 32).toString()} secureTextEntry={true}
+            onChangeText={(value) => setValues(prev => ({ ...prev, password: value }))}
+          />
+          <Input
+            placeholder="Confirmar a palavra-passe"
+            width={(width - 32).toString()} secureTextEntry={true}
+            onChangeText={(value) => setConfirmPassword(value)}
+          />
           <GhostElement/>
           <GhostElement/>
         </>
--- services/sign-in.ts
+++ services/sign-in.ts
@@ -1,17 +1,32 @@
+/* eslint-disable no-undef */
+import { Alert } from 'react-native'
 import { Auth } from '../types/auth.type'
 
 export const signInService = async (email: string, password: string): Promise<Auth> => {
-  return new Promise<Auth>((resolve, reject) => {
-    setTimeout(() => {
-      if (password && email) {
-        resolve({
-          token: '12321324332',
-          email: 'testing@email.com',
-          name: 'John Doe'
-        })
-      } else {
-        reject(new Error('Campos vazios'))
+  const data = { token: '', email: '', name: '' }
+
+  try {
+    const response = await fetch('http://192.168.0.105:8080/api/v1/auth/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password, type: 'driver' }),
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
       }
-    }, 500)
-  })
+    })
+
+    if (response.status === 200) {
+      console.log('201: ', response)
+      const res = await response.json()
+      data.token = res.access_token
+    } else {
+      console.log('N 201: ', response)
+      Alert.alert('Erro', 'Erro abaixo de 500')
+    }
+  } catch (error) {
+    console.log('Erro: ', error)
+    Alert.alert('Erro', 'Erro no catch')
+  }
+
+  return data
 }
