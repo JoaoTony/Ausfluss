1097361959d436860f2ea602f7c0d0da53fee535
JoaoTony <joaojosetony2016@gmail.com>
2022-04-29 05:47:33 +0200

    feat: add crud vehicles

--- screens/home/vehicles/index.tsx
+++ screens/home/vehicles/index.tsx
@@ -1,6 +1,13 @@
 /* eslint-disable no-unused-vars */
 import { useNavigation, useRoute } from '@react-navigation/native'
-import { FC } from 'react'
+import { FC, useState, useEffect } from 'react'
+
+import { Dimensions, Alert, FlatList } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+import Input from '../../../components/input'
+import Button from '../../../components/button'
+import useSwr from 'swr'
 
 import {
   Container,
@@ -12,8 +19,18 @@ import {
   VehicleTitle,
   VehicleHeader,
   VehicleWeight,
-  VehicleStatus
+  VehicleStatus,
+  AddVehicleButton,
+  AddVehicleButtonIcon,
+  AddVehicleContainer,
+  AddVehicleContent,
+  ButtonClose,
+  ButtonCloseIcon,
+  AddVehicleContentForm
 } from './vehicle.styles'
+import { postFetcher } from '../../../services/fetcher'
+
+const { width, height } = Dimensions.get('screen')
 
 interface VehicleProps {
   brand: string;
@@ -21,34 +38,137 @@ interface VehicleProps {
   status: string;
   document: string;
   plate: string;
-  owner: number;
+  owner?: number;
   id: number;
 }
 
 const Vehicles: FC = () => {
   const route = useRoute()
   const navigation = useNavigation()
+  const [showAdd, setShowAdd] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [owner, setOwner] = useState('')
 
-  // const { } = route.params as any
+  const [values, setValues] = useState({
+    brand: '',
+    max_weight_limit: 0,
+    status: '',
+    document: '',
+    plate: ''
+  })
+
+  const createVehicle = async () => {
+    setLoading(true)
+    const { data, status } = await postFetcher('/api/v1/vehicles', values)
+
+    console.log('Porraaaaa', data, status)
+    setLoading(false)
+
+    if (status === 201) {
+      Alert.alert('', 'Veiculo criado com sucesso')
+    } else {
+      Alert.alert('', 'Alguma coisa deu errodo, tente novamente mais tarde')
+    }
+  }
+
+  const { data, error } = useSwr<VehicleProps[]>(`http://192.168.0.105:8080/api/v1/vehicles?owner=${owner}`)
+
+  console.log('Estou testandooooo', data)
+
+  useEffect(() => {
+    (
+      async () => {
+        const authValue = await AsyncStorage.getItem('AusflussAuth')
+        setOwner(JSON.parse(authValue || '').id)
+      }
+    )()
+    return setLoading(false)
+  }, [])
 
   return (
     <Container>
+
+      {showAdd && (
+        <AddVehicleContainer>
+          <AddVehicleContent>
+            <ButtonClose onPress={() => setShowAdd(false)}>
+              <ButtonCloseIcon source={require('../../../assets/icon-close.png')}/>
+            </ButtonClose>
+
+            <AddVehicleContentForm>
+            <Input
+              style={{ marginBottom: 10 }}
+              placeholder="Digite a marca do carro"
+              width={(width - 64).toString()}
+              onChangeText={(e) => setValues(prev => ({ ...prev, brand: e }))}
+            />
+            <Input
+               style={{ marginBottom: 10 }}
+              placeholder="Digete o peso maximo suportado"
+              width={(width - 64).toString()}
+              keyboardType="numeric"
+              onChangeText={(e) => setValues(prev => ({ ...prev, max_weight_limit: Number(e) }))}
+            />
+            <Input
+              style={{ marginBottom: 10 }}
+              placeholder="Digete o estado de conservação"
+              width={(width - 64).toString()}
+              onChangeText={(e) => setValues(prev => ({ ...prev, status: e }))}
+            />
+            <Input
+              style={{ marginBottom: 10 }}
+              placeholder="Digete o código da carta de condução"
+              width={(width - 64).toString()}
+              onChangeText={(e) => setValues(prev => ({ ...prev, document: e }))}
+            />
+            <Input
+              style={{ marginBottom: 10 }}
+              placeholder="Digete o numero da plca"
+              width={(width - 64).toString()}
+              onChangeText={(e) => setValues(prev => ({ ...prev, plate: e }))}
+            />
+
+            <Button
+              width={(width - 64).toString()}
+              onPress={() => createVehicle()}
+              text="Adicionar veiculo"
+              loading={loading}
+            />
+            </AddVehicleContentForm>
+          </AddVehicleContent>
+        </AddVehicleContainer>
+      )}
       <CustomHeader>
         <GoBackButton onPress={() => navigation.goBack()}>
           <GoBackIcon source={require('../../../assets/back-icon.png')}/>
         </GoBackButton>
 
         <Title>Lista de Veiculos</Title>
+
+        <AddVehicleButton onPress={() => setShowAdd(true)}>
+          <AddVehicleButtonIcon source={require('../../../assets/icons-add.png')}/>
+        </AddVehicleButton>
       </CustomHeader>
 
-      <Vehicle>
-        <VehicleHeader>
-         <VehicleTitle>Suzuki</VehicleTitle>
-         <VehicleWeight>2000kg</VehicleWeight>
-        </VehicleHeader>
+      {data && (
+        <FlatList
+          data={data}
+          keyExtractor={item => item.id.toString()}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ marginTop: 40 }}
+          renderItem={({ item }) => (
+            <Vehicle>
+              <VehicleHeader>
+              <VehicleTitle>{item.brand}</VehicleTitle>
+              <VehicleWeight>{item.max_weight_limit}kg</VehicleWeight>
+              </VehicleHeader>
 
-        <VehicleStatus>Bom estado</VehicleStatus>
-      </Vehicle>
+              <VehicleStatus>{item.status}</VehicleStatus>
+            </Vehicle>
+          )}
+        />
+      )}
+
     </Container>
   )
 }
--- screens/home/vehicles/vehicle.styles.ts
+++ screens/home/vehicles/vehicle.styles.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components/native'
 
+import { Dimensions } from 'react-native'
+
+const { width, height } = Dimensions.get('screen')
+
 export const Container = styled.View`
   flex: 1;
   width: 100%;
@@ -16,6 +20,8 @@ export const CustomHeader = styled.View`
   flex-direction: row;
   justify-content: flex-start;
   align-items: flex-end;
+
+  position: relative;
 `
 export const GoBackButton = styled.TouchableOpacity`
   margin-right: 20px;
@@ -36,6 +42,7 @@ export const Vehicle = styled.View`
   background-color: #fff;
   padding: 10px;
   border-radius: 8px;
+  margin-bottom: 10px;
 `
 
 export const VehicleHeader = styled.View`
@@ -63,4 +70,52 @@ export const VehicleWeight = styled.Text`
   font-family: Poppins_400Regular;
   color: #000;
   margin: 0;
+`
+
+export const AddVehicleButton = styled.TouchableOpacity`
+  position: absolute;
+  right: 0;
+  bottom: 5px;
+`
+export const AddVehicleButtonIcon = styled.Image`
+
+`
+
+export const AddVehicleContainer = styled.View`
+  position: absolute;
+  z-index: 20;
+  width: ${width}px;
+  height: ${height}px;
+  flex: 1;
+  background-color: #00000060;
+
+  align-items: center;
+  justify-content: center;
+  padding: 0 16px;
+`
+
+export const AddVehicleContent = styled.View`
+  width: 100%;
+
+  background-color: #fff;
+  border-radius: 8px;
+
+  position: relative;
+
+  padding-bottom: 50px;
+`
+
+export const ButtonClose = styled.TouchableOpacity`
+  position: absolute;
+  right: 16px;
+  top: 16px;
+`
+export const ButtonCloseIcon = styled.Image`
+
+`
+
+export const AddVehicleContentForm = styled.View`
+  width: 100%;
+  margin-top: 60px;
+  align-items: center;
 `
